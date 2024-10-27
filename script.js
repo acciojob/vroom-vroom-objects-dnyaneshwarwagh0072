11ef131e3c47950358ec8f4aa2ed4cfb99bcdb84
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-27 19:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
 
-// Do not change the code below
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+const myCar = new Car('Toyota', 'Corolla');
+console.log(myCar.getMakeModel()); // Output: Toyota Corolla
+
+const mySportsCar = new SportsCar('Ferrari', '488', 211);
+console.log(mySportsCar.getMakeModel()); // Output: Ferrari 488
+console.log(mySportsCar.getTopSpeed()); // Output: 211
+
